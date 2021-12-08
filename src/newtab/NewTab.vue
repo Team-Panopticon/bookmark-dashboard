70faff720cd536e5e5ded103be68d4513ee584f9
juyoungpark718 <juyoung7018.park@gmail.com>
2021-12-08 22:03:32 +0900

    feat: bookmark read api 적용
    - @types/chrome 설치

--- src/newtab/NewTab.vue
+++ src/newtab/NewTab.vue
@@ -1,11 +1,22 @@
 <template lang="">
-  <div>new tab에서 보여지는 페이지 입니다 gaoifjej ewoijafwe</div>
+  <div>{{ JSON.stringify(bookmarks) }}</div>
 </template>
 <script>
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Popup",
+  data: function () {
+    return {
+      bookmarks: [],
+    };
+  },
+  async mounted() {
+    this.bookmarks = await chrome.bookmarks.getTree().then(async (tree) => {
+      const [main, _] = tree[0].children;
+      return await chrome.bookmarks.getChildren(main.id);
+    });
+  },
 });
 </script>
 <style lang="scss"></style>
