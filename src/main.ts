0a4b6acc1448080607c320243497ac250e76e32f
juyoungpark718 <juyoung7018.park@gmail.com>
2021-12-11 11:47:46 +0900

    feat: bookmarks mockdata 추가

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import Popup from "./popup/PopUp.vue";
 import NewTab from "./newtab/NewTab.vue";
+import vuetify from "@/plugins/vuetify";
 
-createApp(NewTab).mount("#app");
+createApp(NewTab).use(vuetify).mount("#app");
