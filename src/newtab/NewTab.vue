4553f6e26e094093a362af845ff73a769a36292e
HojinLee <hojin5633@gmail.com>
2021-12-25 12:02:14 +0900

    refactor: Modal 컴포넌트 분리
    - item 타입 chrome BookmarkTreeNode 적용
    
    - Modal 컴포넌트 분리
    
    - openFolder , openModal event 분리
    
    Co-authored-by: saeeng <saeeng@users.noreply.github.com>
    Co-authored-by: juyoungpark718 <juyoungpark718@users.noreply.github.com>
    Co-authored-by: Junsu Shin <junsushin4546@gmail.com>

--- src/newtab/NewTab.vue
+++ src/newtab/NewTab.vue
@@ -1,22 +1,24 @@
 <template lang="">
-  <Desktop :item="bookmarks"></Desktop>
+  <Desktop :items="bookmarks"></Desktop>
 </template>
-<script>
+
+<script lang="ts">
 import { defineComponent } from "vue";
-import Desktop from "./pages/Desktop";
+import Desktop from "./pages/Desktop.vue";
+import { Item } from "../shared/types/store";
 
 export default defineComponent({
   name: "Popup",
   components: { Desktop },
   data() {
     return {
-      bookmarks: [],
+      bookmarks: [] as Item[],
     };
   },
   async mounted() {
     this.bookmarks = await chrome.bookmarks.getTree().then(async (tree) => {
-      const [main, _] = tree[0].children;
-      return main.children;
+      const [main, _] = tree[0].children || [];
+      return main.children || [];
     });
   },
 });
