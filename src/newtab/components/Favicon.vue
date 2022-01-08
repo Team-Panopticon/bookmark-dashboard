1dc0d2ddad2036a07fe0b074246c70dad2194f0e
juyoungpark718 <juyoung7018.park@gmail.com>
2022-01-08 12:08:57 +0900

    chore: PRE_FIX -> PREFIX로 변경

--- src/newtab/components/Favicon.vue
+++ src/newtab/components/Favicon.vue
@@ -6,7 +6,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
-const PRE_FIX = "https://www.google.com/s2/favicons?domain=";
+const PREFIX = "https://www.google.com/s2/favicons?domain=";
 
 export default defineComponent({
   props: {
@@ -17,7 +17,7 @@ export default defineComponent({
   },
   computed: {
     src() {
-      return PRE_FIX + this.url;
+      return PREFIX + this.url;
     },
   },
 });
