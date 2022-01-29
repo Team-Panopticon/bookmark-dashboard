08219c30c56d602f7e80c932f8a59ec3d9e63bc2
juyoungpark718 <juyoung7018.park@gmail.com>
2022-01-30 01:43:53 +0900

    feat: Bookshelf에 openContextMenu 추가, BookshelfModal에 targetId 추가

--- src/newtab/pages/Desktop.vue
+++ src/newtab/pages/Desktop.vue
@@ -2,21 +2,24 @@
   <Bookshelf
     @openBookshelfModal="openBookshelfModal"
     :items="items"
-    @contextmenu.prevent.stop="openContextMenu($event)"
+    @contextmenu.prevent.stop="
+      openContextMenu($event, { id: '1', type: 'BACKGROUND' })
+    "
   ></Bookshelf>
   <BookshelfModal
-    v-for="({ title, children, showBookshelfModal, zIndex }, i) in modals"
+    v-for="(
+      { targetId, title, children, showBookshelfModal, zIndex }, i
+    ) in modals"
     :key="i"
     @mousedown.capture="focusModal(i)"
     @closeBookshelfModal="closeBookshelfModal($event, i)"
+    :targetId="targetId"
     :initItems="children"
     :initTitle="title"
     :showBookshelfModal="showBookshelfModal"
     :zIndex="zIndex"
   ></BookshelfModal>
-  <ContextMenu>
-    <div class="context-menu-item">Create Folder</div>
-  </ContextMenu>
+  <ContextMenu />
 </template>
 
 <script lang="ts">
@@ -24,13 +27,10 @@ import { defineComponent } from "vue";
 import { Item, modalInfo } from "../../shared/types/store";
 import Bookshelf from "../components/Bookshelf.vue";
 import BookshelfModal from "../components/BookshelfModal.vue";
-import ContextMenu from "../components/ContextMenu.vue";
+import ContextMenu from "../components/ContextMenu/ContextMenu.vue";
 import { mapGetters } from "vuex";
-import store, { GET_BOOKMARK_TREE } from "../store";
-import {
-  SET_CONTEXT_MENU_SHOW_STATE,
-  SET_CONTEXT_MENU_POSITION,
-} from "../store/modules/contextMenu";
+import { GET_BOOKMARK_TREE } from "../store";
+import { openContextMenu } from "../utils/contextMenu";
 
 const OFFSET = 2;
 
@@ -45,10 +45,11 @@ export default defineComponent({
     ...mapGetters({ items: GET_BOOKMARK_TREE }),
   },
   methods: {
-    openBookshelfModal(title: string, children: Item[]) {
+    openBookshelfModal(targetId: string, title: string, children: Item[]) {
       this.modals.push({
-        children: children,
-        title: title,
+        targetId,
+        children,
+        title,
         showBookshelfModal: true,
         zIndex: (this.maxZIndex += OFFSET),
       });
@@ -62,13 +63,7 @@ export default defineComponent({
     focusModal(idx: number) {
       this.modals[idx].zIndex = this.maxZIndex += OFFSET;
     },
-    openContextMenu(event: PointerEvent) {
-      store.commit(SET_CONTEXT_MENU_POSITION, {
-        x: event.clientX,
-        y: event.clientY,
-      });
-      store.commit(SET_CONTEXT_MENU_SHOW_STATE, true);
-    },
+    openContextMenu,
   },
 });
 </script>
@@ -92,13 +87,5 @@ export default defineComponent({
 .modal-banner {
   display: flex;
   justify-content: space-between;
-}
-
-.context-menu-item {
-  padding: 4px 8px;
-  &:hover {
-    background-color: rgba(54, 69, 79, 0.2);
-    cursor: pointer;
-  }
 }
 </style>
